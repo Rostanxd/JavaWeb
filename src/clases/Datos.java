95aa576550893b8fcf28f497aa9db8d4d6eb5036
Rostan <rostanxd@gmail.com>
2016-01-28 16:33:48 -0500

    Creacion de la clase conexion y servlets, Video #4.

---
+++ src/clases/Datos.java
@@ -0,0 +1,51 @@
+package clases;
+
+import java.sql.*;
+
+/**
+ * Created by Rostan on 28/01/2016.
+ */
+public class Datos {
+    private Connection con;
+
+    public Datos(){
+        try {
+            Class.forName("con.mysql.jdbo.Driver");
+            String db = "jdbc:mysql://localhost/facturacion";
+            con = DriverManager.getConnection(db,"root","");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void desconectar(){
+        try {
+            con.close();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean validarUsr(String usu,String pass){
+        Boolean flag = null;
+        try {
+            String sql = "select (1) from usuarios where " +
+                "idUsuario = '' and" +
+                "clave = ' '";
+
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()){
+                flag = true;
+            }else{
+                flag = false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            flag = false;
+        }
+        return flag;
+    }
+}
---
+++ src/servlets/ValidarUsuario.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import java.io.IOException;
+
+/**
+ * Created by Rostan on 28/01/2016.
+ */
+public class ValidarUsuario extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
